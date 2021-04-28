340c9ec8a23d9cc78cdafd74710ae0523e90a20f
Arif Ebrahim <>
2021-04-28 19:26:36 +0100

    refactored step 13 solution

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -4,12 +4,12 @@ class DockingStation
 attr_reader :bike
 
   def release_bike
-    raise "Error: there are no bikes" if @bike.nil? 
+    raise "Error: there are no bikes" unless @bike
     @bike
   end
 
   def dock(bike)
-    raise "Error: no spaces" if !@bike.nil?
+    raise "Error: no spaces" if @bike
     @bike = bike
   end
 end 
