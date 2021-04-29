8017fc52d534885f5ca5cfde03bdf2b505ff9820
Arif Ebrahim <>
2021-04-29 17:46:56 +0100

    completed step 14

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,15 +1,19 @@
 require_relative 'bike'
 
 class DockingStation
-attr_reader :bike
+attr_reader :bikes
+
+  def initialize
+    @bikes = []
+  end
 
   def release_bike
-    raise "Error: there are no bikes" unless @bike
-    @bike
+    raise "Error: there are no bikes" if @bikes.empty?
+    @bikes.pop
   end
 
   def dock(bike)
-    raise "Error: no spaces" if @bike
-    @bike = bike
+    raise "Error: no spaces" if @bikes.length == 20
+    @bikes << bike
   end
 end 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,6 @@
 require 'docking_station'
 
 RSpec.describe DockingStation do
-  # it { is_expected.to respond_to(:release_bike) }
   describe "#release_bike" do
     it "releases a bike" do
       bike = Bike.new
@@ -16,17 +15,15 @@ RSpec.describe DockingStation do
     expect(subject.release_bike).to be_working
   end
 
-  # it { is_expected.to respond_to(:dock).with(1).argument}
   it 'docks a bike' do 
     bike = Bike.new
-    expect(subject.dock(bike)).to eq(bike)
+    expect(subject.dock(bike)).to include(bike)
   end
 
-  # it { is_expected.to respond_to(:bike)}
   it "returns a docked bike" do
     bike = Bike.new
     subject.dock(bike)
-    expect(subject.bike).to eq(bike)
+    expect(subject.release_bike).to eq(bike)
   end
 
   it "raises an error when trying to release a bike when empty" do
@@ -35,7 +32,7 @@ RSpec.describe DockingStation do
 
   describe "#dock" do
     it "raises an error when trying to dock a bike when full" do
-      subject.dock(Bike.new)
+      20.times { subject.dock(Bike.new) }
       expect { subject.dock(Bike.new) }.to raise_error(RuntimeError)
     end
   end
